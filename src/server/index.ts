8555552726dfc1678e3c56023921f36dfbc90653
Noam <noam@fireflymigration.com>
2021-10-24 05:57:54 +0300

    updated swagger

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import { initExpress } from 'remult/server';
 
 let app = express();
-initExpress(app);
+let api = initExpress(app);
+app.use('/api/docs', swaggerUi.serve,
+    swaggerUi.setup(api.openApiDoc({ title: 'remult-react-todo' })));
+
 app.listen(3002, () => console.log("Server started"));
