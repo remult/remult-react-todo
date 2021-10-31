586503013d90f094a27c31c3cc80e227b0b1fff4
Noam <noam@fireflymigration.com>
2021-10-31 12:20:28 +0200

    updated to use graphql

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import { buildSchema } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
+import { remultGraphql } from 'remult/graphql';
 import { initExpress } from 'remult/server';
 
 let app = express();
 let api = initExpress(app);
+
 app.use('/api/docs', swaggerUi.serve,
     swaggerUi.setup(api.openApiDoc({ title: 'remult-react-todo' })));
+
+const { schema, rootValue } = remultGraphql(api);
+app.use('/api/graphql', graphqlHTTP({
+    schema: buildSchema(schema),
+    rootValue,
+    graphiql: true,
+}));
 
 app.listen(3002, () => console.log("Server started"));
